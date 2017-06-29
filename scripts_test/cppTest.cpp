ee0b2ed3b3c111f042e80610fdc26b118c5e210a
ABuarque <abuarquemf@gmail.com>
2017-06-29 03:36:22 -0300

    changed c++ test

--- scripts_test/cppTest.cpp
+++ scripts_test/cppTest.cpp
@@ -1,18 +1,34 @@
-#include "opencv2/opencv.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
-int main(int argc, char** argv) {
-    namedWindow("Output",1);
+//*****************TO CLOSE PRESS ESC*************************
 
-    Mat output = Mat::zeros( 120, 350, CV_8UC3 );
-
-    putText(output, "CRB greatest of Alagoas", cvPoint(15,70),
-            FONT_HERSHEY_PLAIN, 3, cvScalar(0,255,0), 4);
-
-    imshow("Output", output);
-
-    waitKey(0);
+int main(int argc, char* argv[]) {
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cout << "ERROR INITIALIZING VIDEO CAPTURE" << endl;
+	return -1;
+    }
+    
+    string windowName = "Webcam feed";
+    namedWindow(windowName, CV_WINDOW_AUTOSIZE);
+    
+    while(true) {
+        Mat frame;
+        bool success = cap.read(frame);
+        if(!success) {
+            cout << "error" << endl;
+            break;
+        }
+        imshow(windowName, frame);
+        switch(waitKey(10)) {
+            case 27:
+                return 0;
+        }
+    }
     return 0;
 }
