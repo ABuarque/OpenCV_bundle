8b8a5fb8617692f9cea56397bbdd8a32d2dabed8
ABuarque <abuarquemf@gmail.com>
2017-06-30 05:05:34 -0300

    added code to take shot from webcam

---
+++ cpp/take_pic.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    VideoCapture cam = VideoCapture(0);
+    Mat frame;
+    cam >> frame;
+    imwrite("myImage.png", frame);
+    return 0;
+}
