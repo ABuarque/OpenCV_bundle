3ec8f6ecdb4fcfa3d57a7435aa63ceabba34a718
ABuarque <abuarquemf@gmail.com>
2017-12-24 14:41:10 -0300

    added dependencies instller

---
+++ python/resources/dependencies_installer.sh
@@ -0,0 +1,7 @@
+sudo python3.6 -m pip install scipy
+sudo python3.6 -m pip install numpy
+sudo python3.6 -m pip install sklearn
+sudo python3.6 -m pip install pandas
+sudo python3.6 -m pip install matplotlib
+sudo apt-get install python3.6-tk
+sudo python3.6 -m pip install opencv-contrib-python
